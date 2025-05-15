adb83b0f2acb7e233c18a95796da412b0fb504a4
Eric Anderson <eric@booksuey.com>
2025-05-15 09:47:54 +0200

    Add some time testing functionality

--- pages/time.vue
+++ pages/time.vue
@@ -3,23 +3,27 @@
     <h1>Current Date and Time</h1>
     <p id="currentDateTime"></p>
     <p id="formattedDateTime"></p>
+    {{ serverTime }}
   <button @click="updateDateTime">Update Date and Time</button>
   </div>
 </template>
 
 <script setup>
+const serverTime = await useFetch('/api/sessions/time');
+console.log(await useFetch('/sessions/time'));
+console.log("serverTime: ", serverTime);
 const updateDateTime = () => {
   const starttime = new Date().toLocaleTimeString();
   const currentDateTime = new Date();
   const formattedDateTime = currentDateTime.toLocaleString('en-US', {
-    timeZone: 'America/New_York',
+    timeZone: 'America/Detroit',
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
     year: 'numeric',
     month: '2-digit',
     day: '2-digit'
-  });
+  });  
   console.log("starttime: ", starttime);
   console.log("currentDateTime: ", currentDateTime);
   console.log("formattedDateTime: ", formattedDateTime);
